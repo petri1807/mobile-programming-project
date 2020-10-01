394cd53641e929ee38173269d301097ae75ebdf5
petri1807 <petri.lindholm1986@gmail.com>
2020-10-01 11:10:40 +0300

    Branch testi

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Branch testi</Text>
       <Text>FUCK YEAH!!!!½!!</Text>
     </View>
   );
