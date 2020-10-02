50ada9efc89f443eec04d3ae4796d144d0a825b9
kenraalikenobi <ville.lensu@gmail.com>
2020-10-02 15:09:25 +0300

    Calender first steps and install

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-// import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import React, { useState, Fragment } from 'react';
+import { StyleSheet, View, ScrollView, Text, TouchableOpacity } from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
-const CalendarScreen = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>some text</Text>
-  </View>
-);
+const CalendarScreen = () => {
+  const [currentDate, setCurrentDate] = useState('');
+
+  return (
+    <View>
+      <Text style={styles.text} />
+      <Calendar
+        current={Date}
+        style={styles.calendar}
+        hideExtraDays={false}
+        onDayPress={(day) => {
+          console.log('selected day', day);
+        }}
+        firstDay={1}
+        enableSwipeMonths
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  calendar: {
+    marginBottom: 10,
+  },
+  text: {
+    textAlign: 'center',
+    padding: 10,
+    backgroundColor: 'lightgrey',
+    fontSize: 16,
+  },
+});
 
 export default CalendarScreen;
