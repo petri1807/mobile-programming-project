8a85b06bf6dbcc2857e1b2be9f082b05bd5a061a
petri1807 <petri.lindholm1986@gmail.com>
2020-10-05 16:18:35 +0300

    Removed console.logs

--- components/AnnouncementBox.js
+++ components/AnnouncementBox.js
@@ -7,39 +7,27 @@ import {
   addAnnouncement,
 } from '../connection/DBConnection';
 
-// Accordion and it's data to own component
-const dataArray = [
-  {
-    title: 'Prevent COVID-19',
-    content:
-      'Wear Batman masks. Use hand sanitizer. Trump contracted COVID-19, oops.',
-  },
-];
-
 const AnnouncementBox = () => {
   const [loading, setLoading] = useState(true);
   const [announcementList, setAnnouncementList] = useState([
     {
       title: 'Default announcement',
-      content: 'Nothing in database',
+      content: 'Nothing in database.',
     },
   ]);
 
   const fetch = async () => {
     const date = new Date().toDateString();
-    await fetchAllAnnouncements(date)
-      .then((res) => {
-        console.log(res);
-        setAnnouncementList(res.rows._array);
-      })
-      .then(() => console.log(announcementList));
+    await fetchAllAnnouncements(date).then((res) => {
+      setAnnouncementList(res.rows._array);
+    });
   };
 
   const add = async () => {
     const date = new Date().toDateString();
     const title = 'Fire safety exercise at 12:00';
     const content = 'We will conduct a fire safety exercise at 12:00.';
-    await addAnnouncement(date, title, content).then((res) => console.log(res));
+    await addAnnouncement(date, title, content);
   };
 
   useEffect(() => {
