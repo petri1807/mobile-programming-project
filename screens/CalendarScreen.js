2a437b88081bb8e8ec7e40505e8208a6487c846e
kenraalikenobi <ville.lensu@gmail.com>
2020-10-06 16:25:22 +0300

    Calendar agenda updating

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -41,27 +41,28 @@ const CalendarScreen = () => {
   });
 
   const loadItems = (day) => {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
-        if (!items[strTime]) {
-          items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          for (let j = 0; j < numItems; j++) {
-            items[strTime].push({
-              name: `Item for ${strTime} #${j}`,
-              height: Math.max(50, Math.floor(Math.random() * 150)),
-            });
-          }
-        }
-      }
-      const newItems = {};
-      Object.keys(items).forEach((key) => {
-        newItems[key] = items[key];
-      });
-      setItems(newItems);
-    }, 1000);
+    const testiString = 'load items string';
+    // setTimeout(() => {
+    //   for (let i = -15; i < 85; i++) {
+    //     const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+    //     const strTime = timeToString(time);
+    //     if (!items[strTime]) {
+    //       items[strTime] = [];
+    //       const numItems = Math.floor(Math.random() * 3 + 1);
+    //       for (let j = 0; j < numItems; j++) {
+    //         items[strTime].push({
+    //           name: `Item for ${strTime} #${j}`,
+    //           height: Math.max(50, Math.floor(Math.random() * 150)),
+    //         });
+    //       }
+    //     }
+    //   }
+    //   const newItems = {};
+    //   Object.keys(items).forEach((key) => {
+    //     newItems[key] = items[key];
+    //   });
+    //   setItems(newItems);
+    // }, 1000);
   };
 
   const renderItem = (item) => (
@@ -92,8 +93,8 @@ const CalendarScreen = () => {
     <View style={calendarScreen.calendar}>
       <Agenda
         items={items}
-        // loadItemsForMonth={loadItems}
-        selected="2020-10-08"
+        loadItemsForMonth={loadItems}
+        selected={Date}
         firstDay={1}
         renderItem={renderItem}
         onDayPress={(day) => {
