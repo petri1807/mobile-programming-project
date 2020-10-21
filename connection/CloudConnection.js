ef31a252e0bf159205e2f9491acc2180e841a15f
HeikkiLu <heikki.luukkonen@gmail.com>
2020-10-21 23:34:19 +0300

    Activities now connected to cloud

--- connection/CloudConnection.js
+++ connection/CloudConnection.js
@@ -1,5 +1,4 @@
-const urlString =
-  'https://reactnativeprojectrest.appspot.com/rest/calendarservice';
+const urlString = 'https://reactnativeprojectrest.appspot.com/rest';
 
 export const fetchAllCalendarEvents = async () => {
   const eventFetcher = await fetch(`${urlString}/getallcalendarevents`)
@@ -17,4 +16,38 @@ export const fetchAllPlayer = async () => {
       console.log(responseJson);
       return responseJson;
     });
+};
+
+export const fetchAllActivities = async () => {
+  const activitiesFetcher = await fetch(
+    `${urlString}/activityservice/getallactivities`
+  )
+    .then((response) => response.json())
+    .then((responseJson) => responseJson);
+  return activitiesFetcher;
+};
+
+export const addActivity = async (
+  userId,
+  date,
+  timeStart,
+  timeEnd,
+  timeSpent,
+  activityType
+) => {
+  await fetch(`${urlString}/activityservice/addactivity`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      userId: 0,
+      date,
+      timeStart,
+      timeEnd,
+      timeSpent,
+      activityType,
+    }),
+  });
+  console.log('New activity added, maybe....');
 };
